2db790a2b037edfccd249cbcbfa2180786d0f4c2
Amber Bhatt <amberb220@gmail.com>
2019-08-29 13:27:46 -0600

    initial commit

---
+++ js/main.js
@@ -0,0 +1,127 @@
+function back_home(x,y) {
+  window.scrollTo(x,y);
+}
+
+function to_allProjects(x,y) {
+  window.scrollTo(x,y);
+}
+
+function to_projects(x,y) {
+  window.scrollTo(x,y);
+}
+
+function to_contact() {
+  window.open("https://github.com/amber1bhatt/Tech-Resume/blob/master/Update%208-13-2019/TechResumeUBC%20(1).pdf");
+}
+
+// $(document).ready(function() {
+//   $("#button").click(function() {
+//     $("#pro1").show();
+//   });
+// });
+
+
+//OLD PROJECTS TEMPLATE
+// function previewPro1() {
+//   //button = document.getElementById("btn-previewPro1");
+//   iframe = document.getElementById("iframepro1");
+//   iframe.style.display = "inline";
+//   p = document.getElementById("paragraph1");
+//   p.parentNode.removeChild(p);
+// }
+//
+// function previewText1() {
+//   button = document.getElementById("btn-text1");
+//   iframe = document.getElementById("iframepro1");
+//   iframe.style.display = "none";
+//   p = document.createElement("p");
+//   p.setAttribute("id", "paragraph1");
+//   node = document.createTextNode("This is a pseudo machine learning project that was written in JavaScript in which dots are spawned and over time learn to move towards an objective.");
+//   p.appendChild(node);
+//   p.style.paddingTop = "20px";
+//   div = document.getElementById("pro1");
+//   div.appendChild(p);
+// }
+//
+// function previewPro2() {
+//   //button = document.getElementById("btn-previewPro1");
+//   iframe = document.getElementById("iframepro2");
+//   iframe.style.display = "inline";
+//   p = document.getElementById("paragraph2");
+//   p.parentNode.removeChild(p);
+// }
+//
+// function previewText2() {
+//   button = document.getElementById("btn-text2");
+//   iframe = document.getElementById("iframepro2");
+//   iframe.style.display = "none";
+//   p = document.createElement("p");
+//   p.setAttribute("id", "paragraph2");
+//   node = document.createTextNode("Created an AI that the user can play Connect Four against. The intelligence was formed through the implementation of the Minimax Algorithm and Alpha-beta Pruning.");
+//   p.appendChild(node);
+//   p.style.paddingTop = "20px";
+//   div = document.getElementById("pro2");
+//   div.appendChild(p);
+// }
+//
+// function previewPro3() {
+//   //button = document.getElementById("btn-previewPro1");
+//   iframe = document.getElementById("iframepro3");
+//   iframe.style.display = "inline";
+//   p = document.getElementById("paragraph3");
+//   p.parentNode.removeChild(p);
+// }
+//
+// function previewText3() {
+//   button = document.getElementById("btn-text3");
+//   iframe = document.getElementById("iframepro3");
+//   iframe.style.display = "none";
+//   p = document.createElement("p");
+//   p.setAttribute("id", "paragraph3");
+//   node = document.createTextNode("A simple To-Do list in which the user can add, remove and complete tasks. It also makes use of localStorage to store any tasks the user creates.");
+//   p.appendChild(node);
+//   p.style.paddingTop = "20px";
+//   div = document.getElementById("pro3");
+//   div.appendChild(p);
+// }
+//
+// function previewPro4() {
+//   //button = document.getElementById("btn-previewPro1");
+//   iframe = document.getElementById("iframepro4");
+//   iframe.style.display = "inline";
+//   p = document.getElementById("paragraph4");
+//   p.parentNode.removeChild(p);
+// }
+//
+// function previewText4() {
+//   button = document.getElementById("btn-text4");
+//   iframe = document.getElementById("iframepro4");
+//   iframe.style.display = "none";
+//   p = document.createElement("p");
+//   p.setAttribute("id", "paragraph4");
+//   node = document.createTextNode("Recreation of John Conway's Game of Life. A zero-player game which simulates aspects of Overpopulation, Reproduction, and Underpopulation.");
+//   p.appendChild(node);
+//   p.style.paddingTop = "20px";
+//   div = document.getElementById("pro4");
+//   div.appendChild(p);
+// }
+
+//
+function openGOL() {
+  window.open("https://github.com/amber1bhatt/javascriptProjects/tree/master/gameOfLife");
+}
+
+//
+function openC4A() {
+  window.open("https://github.com/amber1bhatt/pythonProjects/tree/master/connectFourAI/dist");
+}
+
+//
+function openTD() {
+  window.open("https://github.com/amber1bhatt/Processing-and-p5/tree/master/thinkingDots");
+}
+
+//
+function openTODO() {
+  window.open("https://github.com/amber1bhatt/ToDo-App");
+}
