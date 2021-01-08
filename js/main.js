909049ce0cbb560ae2d425b3d920a83417112df7
amber1bhatt <amber.bhatt@ubcorbit.com>
2021-01-07 18:48:44 -0700

    revamped and more simplistic

--- js/main.js
+++ js/main.js
@@ -1,14 +1,6 @@
 /*eslint-env es6*/
 
-function back_home(x,y) {
-  window.scrollTo(x,y);
-}
-
-function to_allProjects(x,y) {
-  window.scrollTo(x,y);
-}
-
-function to_projects(x,y) {
+function move(x,y) {
   window.scrollTo(x,y);
 }
 
@@ -16,43 +8,40 @@ function to_contact() {
     window.open("https://github.com/amber1bhatt/Tech-Resume/blob/master/Job%20Search%20Resume/Software%20Resume/Update%202-17-2020/Software2172020.pdf");
 }
 
+
 const darkButton = document.getElementById('dark');
 const lightButton = document.getElementById('light');
+
+const sun_moon = document.getElementById('sun');
+
 const body = document.body;
 
 const theme = localStorage.getItem('theme');
+const icon = localStorage.getItem('icon');
 
 if(theme) {
 	body.classList.add(theme);
 }
 
-darkButton.onclick = () => {
-	body.classList.replace('light', 'dark');
-	localStorage.setItem('theme', 'dark');
-};
-
-lightButton.onclick = () => {
-	body.classList.replace('dark', 'light');
-	localStorage.setItem('theme', 'light');
-};
-
-
-//
-function openGOL() {
-  window.open("https://github.com/amber1bhatt/javascriptProjects/tree/master/gameOfLife");
+if(icon) {
+  document.getElementById('sun_icon').className = icon;
 }
 
-//
-function openC4A() {
-  window.open("https://github.com/amber1bhatt/pythonProjects/tree/master/connectFourAI/dist");
+sun_moon.onclick = () => {
+  if(body.classList == 'light') {
+    body.classList.replace('light', 'dark');
+    localStorage.setItem('theme', 'dark');
+    document.getElementById('sun_icon').className = "fas fa-moon";
+    localStorage.setItem('icon', 'fas fa-moon');
+  }
+  else {
+    body.classList.replace('dark', 'light');
+    localStorage.setItem('theme', 'light');
+    document.getElementById('sun_icon').className = "fas fa-sun";
+    localStorage.setItem('icon', 'fas fa-sun');
+  }
 }
 
-//
-function openTD() {
-  window.open("https://github.com/amber1bhatt/Processing-and-p5/tree/master/thinkingDots");
-}
-
-//
-function openTODO() {
-  window.open("https://github.com/amber1bhatt/ToDo-App");
+function openGithub(url) {
+  window.open(url);
 }
