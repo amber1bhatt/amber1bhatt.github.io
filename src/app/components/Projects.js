d86857fffdd65040a20f916baf85cdae13db5422
Amber Bhatt <amberb220@gmail.com>
2024-01-31 13:16:03 -0800

    tiny code removed

--- src/app/components/Projects.js
+++ src/app/components/Projects.js
@@ -81,7 +81,7 @@ const Projects = ({ projects }) => {
                 strokeWidth="2"
                 d="M5 10l7-7m0 0l7 7m-7-7v18"
               />
-            </svg>{" "}
+            </svg>
           </button>
         </div>
       </div>
