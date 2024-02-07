15ad9a278fa1779b87d5137270ff6a7c2f1981dd
Amber Bhatt <amberb220@gmail.com>
2024-02-07 14:24:58 -0800

    added deployment links

--- src/app/components/Card.js
+++ src/app/components/Card.js
@@ -1,7 +1,8 @@
 import React from "react";
 import Image from "next/image";
+import GitHubIcon from "../icons/GitHubIcon";
 
-const Card = ({ imageUrl, name, description, githubLink }) => {
+const Card = ({ imageUrl, name, description, githubLink, deploymentLink }) => {
   return (
     <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 overflow-hidden group relative transition-transform ease-in-out duration-300 hover:scale-105">
       <a href={githubLink} target="_blank" rel="noopener noreferrer">
@@ -22,29 +23,32 @@ const Card = ({ imageUrl, name, description, githubLink }) => {
         <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
           {description}
         </p>
-        <a
-          href={githubLink}
-          target="_blank"
-          rel="noopener noreferrer"
-          className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-        >
-          Learn more
-          <svg
-            className="rtl:rotate-180 w-3.5 h-3.5 ms-2"
-            aria-hidden="true"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 14 10"
+        <div className="flex justify-between items-center">
+          <a
+            href={deploymentLink ?? githubLink}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
           >
-            <path
-              stroke="currentColor"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M1 5h12m0 0L9 1m4 4L9 9"
-            />
-          </svg>
-        </a>
+            Learn more
+            <svg
+              className="rtl:rotate-180 w-3.5 h-3.5 ms-2"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 14 10"
+            >
+              <path
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M1 5h12m0 0L9 1m4 4L9 9"
+              />
+            </svg>
+          </a>
+          <GitHubIcon url={githubLink} />
+        </div>
       </div>
     </div>
   );
